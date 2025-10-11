6ad541aa1ab8031898a0033e00cdae234f276a14
unknown <blkilpatrick42@gmail.com>
2025-10-11 11:20:46 -0500

    added patrol point web for northeastern map plus QOL improvements for patrol point tool scripting

--- scripts/character/mobsters/patrol_point.gd
+++ scripts/character/mobsters/patrol_point.gd
@@ -2,7 +2,6 @@
 extends Node2D
 
 @export var next_points : Array[Node] = []
-@export var prev_point = Node2D
 @export var has_prev_point = false
 
 var random = RandomNumberGenerator.new()
@@ -17,8 +16,6 @@ func has_next_point():
 func get_next_point():
 	if(has_next_point()):
 		return next_points[randi_range(0, next_points.size() - 1)]
-	else:
-		return prev_point
 
 func is_occupied():
 	var mobsters = get_tree().get_nodes_in_group("mobster")
@@ -32,9 +29,17 @@ func _draw():
 		if(has_next_point()):
 			for point in next_points:
 				if(point != null):
-					draw_line(Vector2(), get_transform().affine_inverse() * point.position, Color(1,0,0,1), -1)
-		if(has_prev_point):
-			draw_line(Vector2(), get_transform().affine_inverse() * prev_point.position, Color(0,0,1,1), -1)
+					if(point.has_next_point()):
+						if(point.next_points.find(self) >= 0):
+							draw_line(Vector2(), get_transform().affine_inverse() * point.position, Color(0,1,0,1), -1)
+						else:
+							var other_point = get_transform().affine_inverse() * point.position
+							draw_line(Vector2(), other_point, Color(1,0,0,1), -1)
+							draw_line(other_point, Vector2(other_point.x+16, other_point.y+16),Color(1,0,0,1),-1)
+					else:
+						var other_point = get_transform().affine_inverse() * point.position
+						draw_line(Vector2(), other_point, Color(1,0,0,1), -1)
+						draw_line(other_point, Vector2(other_point.x+16, other_point.y+16),Color(1,0,0,1),-1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

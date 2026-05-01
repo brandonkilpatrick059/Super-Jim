141e9aa75e5f9d02f8621910e2f7290f625401cf
unknown <blkilpatrick42@gmail.com>
2026-05-01 17:47:27 -0500

    added different cat colors

--- scripts/character/NPC/fauna/npc_cat_interact.gd
+++ scripts/character/NPC/fauna/npc_cat_interact.gd
@@ -1,11 +1,16 @@
 extends Node
 
 @export var pickupable_cat_path : String = ""
+@export var pickupable_cat_path_docile : String = ""
 
 func interact():
 	var player_ref = get_tree().get_first_node_in_group("player")
 	if(not player_ref.get_holding_object()):
-		var pickupable_cat = load(pickupable_cat_path)
+		var pickupable_cat
+		if(get_parent().find_child("ai_state_machine").get_state().name == "eating"):
+			pickupable_cat = load(pickupable_cat_path_docile)
+		else:
+			pickupable_cat = load(pickupable_cat_path)
 		var pick_cat = pickupable_cat.instantiate()
 		get_parent().get_parent().add_child(pick_cat)
 		pick_cat.pick_up(player_ref)

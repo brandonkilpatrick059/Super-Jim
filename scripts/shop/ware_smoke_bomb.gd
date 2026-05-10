5affcfa007669d26bc03e609f4558f544193c572
unknown <blkilpatrick42@gmail.com>
2026-05-10 17:18:59 -0500

    npc kyle now plays cards to open up smoke bomb shop

---
+++ scripts/shop/ware_smoke_bomb.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func run_script():
+	var player_ref = get_tree().get_first_node_in_group("player")
+	player_ref.add_smoke_bombs(1)
+	var sound_player = get_tree().get_first_node_in_group("main_fx_player")
+	sound_player.stream = load("res://audio/soundFX/dash_regen.wav")
+	sound_player.play()

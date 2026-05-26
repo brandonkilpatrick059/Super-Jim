9486757c4d34c6fb3d23ffb07eeb11bdf2047f45
unknown <blkilpatrick42@gmail.com>
2026-05-25 20:44:48 -0500

    misc tweaks + fixes

--- scripts/utility/run_conditionals/has_schedules_index.gd
+++ scripts/utility/run_conditionals/has_schedules_index.gd
@@ -5,7 +5,8 @@ extends Node
 
 func run_conditional():
 	var check_node = get_tree().get_first_node_in_group(node_group)
-	if(check_node.get_schedules_key() == check_key):
+	var current_key = check_node.get_schedules_key()
+	if(current_key == check_key):
 		return 1
 	else:
 		return 0

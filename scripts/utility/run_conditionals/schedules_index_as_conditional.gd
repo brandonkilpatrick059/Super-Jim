dc66ed02e342a6d546c37a5da605e1f0527fb530
Brandon Kilpatrick <brandonkilpatrick059@gmail.com>
2026-06-03 11:19:34 -0500

    sebastian quests finished up to the hat quest

--- scripts/utility/run_conditionals/schedules_index_as_conditional.gd
+++ scripts/utility/run_conditionals/schedules_index_as_conditional.gd
@@ -6,6 +6,6 @@ extends Node
 
 func run_conditional():
 	var check_node = get_tree().get_first_node_in_group(node_group)
-	var current_key = check_node.get_schedules_index()
+	var current_key = check_node.get_schedules_key()
 	var index = check_keys.find(current_key)
 	return conditional_return_map[index]

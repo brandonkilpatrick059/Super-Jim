3b57d4a34b0cc2550489c78af17dddab907aeeae
Brandon Kilpatrick <brandonkilpatrick059@gmail.com>
2026-06-07 12:34:18 -0500

    set default lighting setting to medium

--- scripts/menus/settings_variables.gd
+++ scripts/menus/settings_variables.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var lighting_settings = ["LOW", "MEDIUM", "HIGH"]
-var lighting_index = 0
+var lighting_index = 1
 var supported_resolutions = [Vector2(632,320),Vector2(600,360),Vector2(640,360)]
 var resolution_index = 0
 var full_screen = false
